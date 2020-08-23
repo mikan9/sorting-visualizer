802bca1edab99ffdc3fde921c4728d356cb313e6
Stephanie Ahlström <ahlstrom.stephanie.se@gmail.com>
2020-08-23 19:10:14 +0200

    Updates

--- precache-manifest.ddc29b97428e54f2b9a1d0a048afa989.js
+++ precache-manifest.ddc29b97428e54f2b9a1d0a048afa989.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "9d3f61c50f77c9c0bccdf8b79ffe9944",
+    "revision": "b5136de7cdba604c9c324ae05bec1af8",
     "url": "/sorting-visualizer/index.html"
   },
   {
-    "revision": "ed5fef2c7c1b74ecd7f3",
-    "url": "/sorting-visualizer/static/css/main.9c44cf97.chunk.css"
+    "revision": "6b193430a8973b04eb4d",
+    "url": "/sorting-visualizer/static/css/main.9603889d.chunk.css"
   },
   {
     "revision": "1540c09237f27894f7dc",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/sorting-visualizer/static/js/2.a6686700.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "ed5fef2c7c1b74ecd7f3",
-    "url": "/sorting-visualizer/static/js/main.55a4f96b.chunk.js"
+    "revision": "6b193430a8973b04eb4d",
+    "url": "/sorting-visualizer/static/js/main.d5aadaf5.chunk.js"
   },
   {
     "revision": "0a5bbf806931994df848",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/sorting-visualizer/precache-manifest.86a0af8961c80d9d33fefe896a249736.js"
+  "/sorting-visualizer/precache-manifest.ddc29b97428e54f2b9a1d0a048afa989.js"
 );
 
 self.addEventListener('message', (event) => {
